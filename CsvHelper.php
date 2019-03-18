7ece91e138779ea5ba20b9b93553bc7ac49ec273
d52b8 <iluhap@yandex.ru>
2019-03-18 14:52:17 +0300

    Выполянет инициализацию

---
+++ CsvHelper.php
@@ -0,0 +1,59 @@
+<?php
+namespace d52b8;
+
+class CsvHelper
+{
+    private static function encode($text)
+    {
+        if (mb_detect_encoding($text, 'Windows-1251', true) == 'Windows-1251') {
+            return iconv('cp1251', "utf-8", $text);
+        }
+        return $text;
+        // return iconv(mb_detect_encoding($text, mb_detect_order(), true), "utf-8", $text);
+    }
+
+    private static function removeBom($text) {
+        if (substr($text, 0, 3) == pack('CCC', 0xef, 0xbb, 0xbf)) {
+            $text = substr($text, 3);
+        }
+        return $text;
+    }
+
+    public static function test()
+    {
+        echo "Hello, World!";
+    }
+
+    public static function read($path)
+    {
+        try {
+            $file = new \SplFileObject($path);
+            $file->setFlags(
+                \SplFileObject::READ_CSV |
+                \SplFileObject::SKIP_EMPTY |
+                \SplFileObject::READ_AHEAD
+            );
+            $file->setCsvControl(';',"\"");
+            $header = false;
+            while (!$file->eof()) {
+                $row = $file->current();
+                $row = array_map('self::encode', $row);
+                $row = array_map('trim', $row);
+                $file->next();
+                if ($row) {
+                    if (!$header) {
+                        $row = array_map('self::removeBom', $row);
+                        $header = $row;
+                        continue;
+                    }
+                    if (count($header) == count($row)) {
+                        $row = array_combine($header, $row);
+                        yield $row;
+                    }
+                }
+            }
+        } catch (Exception $e) {
+            
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+# csv-helper
+
+### Добавить репозиторий в `composer.json`
+```
+"repositories": [
+    {
+        "type": "git",
+        "url": "https://github.com/d52b8/csv-helper.git"
+    }
+]
+```
+
+### Подключить библиотеку CsvHelper
+```
+composer require "d52b8/csv-helper"
+```
+
+### Использовать библиотеку CsvHelper
+```php
+<?php
+use d52b8\CsvHelper;
+
+$csv = CsvHelper::read('/path/to/file.csv');
+foreach ($csv as $row) {
+    print_r($csv);
+}
+```
